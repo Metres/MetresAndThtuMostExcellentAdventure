9f299915786cfd8b777dca63fa87842e2bfdcca2
Thomas Tu <t3tu@ucsd.edu>
2014-10-05 14:56:46 -0700

    Fixed vecfield

--- vecfield.py
+++ vecfield.py
@@ -8,17 +8,17 @@ fig = plt.figure(num=1)
 ax=fig.add_subplot(111)
 
 #Vector field
-X,Y = np.meshgrid( np.linspace(-2,2,20),np.linspace(0,4,20) )
+X,Y = np.meshgrid( np.linspace(0,4,20),np.linspace(-2,2,20) )
 U = 1
-V = np.sqrt(U)*(X**2)
+V = np.sqrt(X)*(Y**2)
 #Normalize arrows
 N = np.sqrt(U**2+V**2)  
 U2, V2 = U/N, V/N
 ax.quiver( X,Y,U2, V2)
 
 
-plt.xlim([-2,2])
-plt.ylim([0,4])
+plt.ylim([-2,2])
+plt.xlim([0,4])
 plt.xlabel(r"$x$")
 plt.ylabel(r"$y$")
 plt.show()
